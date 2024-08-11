db002e2a5d961d5281bf9901b00ddc2a5da94343
Tue Minh <178058439+TueMinh-Sunna@users.noreply.github.com>
2024-08-11 13:23:08 +0700

    lab1 welcome again

---
+++ Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome {
+    public static void main(String[] args) {
+        String hoten = "Thai Nguyen Tue Minh";
+        long lopHP = 422000411904L;
+
+        System.out.println("Hello " + hoten + "va " + lopHP);
+    }
+}
